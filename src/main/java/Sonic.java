fca82a9dac8455f1099fba58b8337d1d1696c666
theertha120 <theertha120@gmail.com>
2025-01-24 00:49:45 +0800

    Level 0: Rename, Greet, Exit

---
+++ src/main/java/Sonic.java
@@ -0,0 +1,16 @@
+public class Sonic {
+    public static void main(String[] args) {
+        String logo = " SSSSS  OOOOO  N   N  III  CCCCC \n"
+                + " S      O   O  NN  N   I   C     \n"
+                + "  SSS   O   O  N N N   I   C     \n"
+                + "     S  O   O  N  NN   I   C     \n"
+                + " SSSSS  OOOOO  N   N  III  CCCCC \n";
+
+        System.out.println("Hello from\n" + logo);
+        String line = "__________________________________________________________";
+        System.out.println("I am Sonic! What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
