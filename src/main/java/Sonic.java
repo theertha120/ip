508535707ea63dba7edc736fcef7f1ee94df8633
theertha120 <theertha120@gmail.com>
2025-01-31 01:13:28 +0800

    add tasks and display list

--- src/main/java/Sonic.java
+++ src/main/java/Sonic.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Sonic {
     public static void main(String[] args) {
@@ -14,14 +15,30 @@ public class Sonic {
         System.out.println(line);
 
         Scanner userInputScanner = new Scanner(System.in);
+        String[] tasksList = new String[100];
+        int tasksCount = 0;
+
         while (true) {
             String userInput = userInputScanner.nextLine();
+
             if (userInput.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(line);
                 break;
+            } else if (userInput.equalsIgnoreCase("list")) {
+                if (tasksCount == 0) {
+                    System.out.println("No tasks to show.");
+                } else {
+                    for (int i = 0; i < tasksCount; i++) {
+                        System.out.println((i + 1) + ". " + tasksList[i]);
+                    }
+                }
+            } else {
+                tasksList[tasksCount] = userInput;
+                tasksCount++;
+                System.out.println("added: " + userInput);
             }
-            System.out.println(userInput);
+
             System.out.println(line);
         }
 
