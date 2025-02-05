1ec8a14ea29beff4e3032763bb323901aca2a19d
theertha120 <theertha120@gmail.com>
2025-02-05 21:36:45 +0800

    add Todo, Event, Deadline functionalities

--- src/main/java/Sonic.java
+++ src/main/java/Sonic.java
@@ -1,13 +1,23 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Sonic {
+    private static int addTask(Task[] tasksList, int tasksCount, Task task) {
+        tasksList[tasksCount] = task;
+        tasksCount++;
+        System.out.println("Got it, I have added this task:");
+        System.out.println("   " + task.toString());
+        System.out.println("Now you have " + tasksCount + " tasks in the list.");
+        return tasksCount;
+    }
+
     public static void main(String[] args) {
-        String logo = " SSSSS  OOOOO  N   N  III  CCCCC \n"
-                + " S      O   O  NN  N   I   C     \n"
-                + "  SSS   O   O  N N N   I   C     \n"
-                + "     S  O   O  N  NN   I   C     \n"
-                + " SSSSS  OOOOO  N   N  III  CCCCC \n";
+        String logo = """
+        SSSSS  OOOOO  N   N  III  CCCCC 
+        S      O   O  NN  N   I   C     
+         SSS   O   O  N N N   I   C     
+            S  O   O  N  NN   I   C     
+        SSSSS  OOOOO  N   N  III  CCCCC""";
+
 
         System.out.println("Hello from\n" + logo);
         String line = "__________________________________________________________";
@@ -32,24 +42,24 @@ public class Sonic {
                 } else {
                     System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < tasksCount; i++) {
-                        System.out.println((i + 1) + ".[" + tasksList[i].getStatusIcon() + "] " + tasksList[i].getDescription());
+                        System.out.println((i + 1) + "." + tasksList[i].toString());
                     }
                 }
 
             } else if (userInput.toLowerCase().startsWith("add")) {
                 String taskText = userInput.substring(4);
-                tasksList[tasksCount] = new Task(taskText);
-                tasksCount++;
-                System.out.println("added: " + taskText);
+                Task newTask = new Task(taskText);
+                tasksCount = addTask(tasksList, tasksCount, newTask);
 
             } else if (userInput.toLowerCase().startsWith("mark")) {
                 String[] splitUserInput = userInput.split(" ");
                 int taskNumber = Integer.parseInt(splitUserInput[1]);
+
                 if (taskNumber >= 1 && taskNumber <= tasksCount) {
                     Task taskToMark = tasksList[taskNumber - 1];
                     taskToMark.setDone(true);
                     System.out.println("Nice! I've marked this task as done:");
-                    System.out.println("  [" + taskToMark.getStatusIcon() + "] " + taskToMark.getDescription());
+                    System.out.println(taskToMark);
                 } else {
                     System.out.println("Invalid task number!");
                 }
@@ -57,14 +67,41 @@ public class Sonic {
             } else if (userInput.toLowerCase().startsWith("unmark")) {
                 String[] splitUserInput = userInput.split(" ");
                 int taskNumber = Integer.parseInt(splitUserInput[1]);
+
                 if (taskNumber >= 1 && taskNumber <= tasksCount) {
                     Task taskToUnmark = tasksList[taskNumber - 1];
                     taskToUnmark.setDone(false);
                     System.out.println("Ok, I've marked this task as not done yet:");
-                    System.out.println("  [" + taskToUnmark.getStatusIcon() + "] " + taskToUnmark.getDescription());
+                    System.out.println(taskToUnmark);
                 } else {
                     System.out.println("Invalid task number!");
                 }
+
+            } else if (userInput.toLowerCase().startsWith("todo")){
+                String taskText = userInput.substring(5);
+
+                Todo newTodo = new Todo(taskText);
+                tasksCount = addTask(tasksList, tasksCount, newTodo);
+
+            } else if (userInput.toLowerCase().startsWith("event")) {
+                String[] fromSplit = userInput.split(" /from ");
+                String[] toSplit = fromSplit[1].split(" /to ");
+
+                String eventName = fromSplit[0].substring(6).trim();
+                String startTime = toSplit[0].trim();
+                String endTime = toSplit[1].trim();
+
+                Event newEvent = new Event(eventName, startTime, endTime);
+                tasksCount = addTask(tasksList, tasksCount, newEvent);
+
+            } else if (userInput.toLowerCase().startsWith("deadline")) {
+                String[] userInputSplit = userInput.split(" /by ");
+
+                String deadlineName = userInputSplit[0].substring(9).trim();
+                String endTime = userInputSplit[1].trim();
+
+                Deadline newDeadline = new Deadline(deadlineName, endTime);
+                tasksCount = addTask(tasksList, tasksCount, newDeadline);
 
             } else {
                 System.out.println("Unknown command: " + userInput);
