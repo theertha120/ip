b8d51ec8936d1f6f8e8c993e82e105f4b77befa4
theertha120 <theertha120@gmail.com>
2025-03-06 13:13:19 +0800

    add date and time formats

--- src/main/java/sonic/ui/Event.java
+++ src/main/java/sonic/ui/Event.java
@@ -1,24 +1,46 @@
 package sonic.ui;
 
+/**
+ * Represents an Event task, a type of Task with a description, start time, and end time.
+ */
 public class Event extends Task {
 
     protected String start;
     protected String end;
 
+    /**
+     * Initializes an Event task with the description, start time, and end time.
+     * The task is initially marked as not done.
+     *
+     * @param description The description of the Event task.
+     * @param start The start time of the event (in string format).
+     * @param end The end time of the event (in string format).
+     */
     public Event(String description, String start, String end) {
         super(description);
-        this.start = start;
-        this.end = end;
+        this.start = DateTime.formatInputDateTime(start);
+        this.end = DateTime.formatInputDateTime(end);
     }
 
+    /**
+     * Returns a string representation of the Event task.
+     * The format is "[E][StatusIcon] description (from: start to: end)".
+     *
+     * @return A string representation of the Event task.
+     */
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (from: " + start + " to: " + end + ")";
     }
 
+    /**
+     * Returns a string representation of the Event task in a format suitable for saving to a file.
+     * The format is: "E | isDone (1 or 0) | description | start | end".
+     *
+     * @return A string suitable for saving the Event task to a file.
+     */
     @Override
     public String toFileString() {
         return "E | " + (isDone ? "1" : "0") + " | " + description + " | " + start + " | " + end;
     }
-
 }
--- src/main/java/sonic/ui/Todo.java
+++ src/main/java/sonic/ui/Todo.java
@@ -1,16 +1,37 @@
 package sonic.ui;
 
+/**
+ * Represents a Todo task, a type of Task with a description.
+ */
 public class Todo extends Task {
 
+    /**
+     * Initializes a Todo task with the given description.
+     * The task is initially marked as not done.
+     *
+     * @param description The description of the Todo task.
+     */
     public Todo(String description) {
         super(description);
     }
 
+    /**
+     * Returns a string representation of the Todo task.
+     * The format is "[T][StatusIcon] description".
+     *
+     * @return A string representation of the Todo task.
+     */
     @Override
     public String toString() {
         return "[T]" + super.toString();
     }
 
+    /**
+     * Returns a string representation of the Todo task in a format suitable for saving to a file.
+     * The format is: "T | isDone (1 or 0) | description".
+     *
+     * @return A string suitable for saving the Todo task to a file.
+     */
     @Override
     public String toFileString() {
         return "T | " + (isDone ? "1" : "0") + " | " + description;
